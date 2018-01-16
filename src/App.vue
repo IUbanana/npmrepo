3407f13de9bb8bdc64bb5a1a78c7c821c1504423
banana <banana@bananadeMacBook-Pro.local>
2018-01-16 20:09:37 +0800

    vue-runoob

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,85 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+
+    <el-button @click="visible = true">按钮</el-button>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>欢迎使用 Element</p>
+    </el-dialog>
+
     <!-- 标签形式插入组件 -->
+    <globalcomp></globalcomp>
+    <input v-model="parentMsg">
+    <localcomp v-bind:msg="parentMsg"></localcomp>
     <comptest></comptest>
     <runoobcomp></runoobcomp>
   </div>
 </template>
 
 <!-- 脚本代码，写JavaScript -->
+<script src="//unpkg.com/vue/dist/vue.js"></script>
+<script src="//unpkg.com/element-ui/lib/index.js"></script>
 <script>
+//导入Vue
+import Vue from 'vue'
+import Element from 'element-ui'
 //导入组件
 import comptest from './components/CompTest'
 import runoobcomp from './components/runoobcomp'
 
-export default {
-  name:'app',
-  components:{ 
+Vue.use(Element, { size: 'small' })
+
+var Main = {
+  data() {
+    return {
+      visible: false
+    }
+  }
+}
+var Ctor = Vue.extend(Main)
+new Ctor().$mount('#app')
+
+//注册全局组件，可在html中直接标签导入
+Vue.component('globalcomp',{
+  template:'<h2>自定义全局组件</h2>'
+  //声明props，传递数据
+})
+
+//定义局部组件
+var local = {
+  // template: '<h2>自定义局部组件</h2>',
+  props: ['msg'],
+  template: '<span>props传递的数据：{{msg}}</span>'
+}
+
+export default{
+  // name:'app',
+  data () {
+    return {
+      parentMsg: 'parentContent'
+    }
+  },
+  components: { 
+    //注册局部组件
+    'localcomp': local,
+    //import的组件也要注册
     comptest,
     runoobcomp
   }
+
 }
-
-
 </script>
 
 <!-- 样式代码，写CSS -->
 <style>
-#app {
+@import url("//unpkg.com/element-ui/lib/theme-chalk/index.css");
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
+
 </style>
--- src/components/runoobcomp.vue
+++ src/components/runoobcomp.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="runoobcomp">
-    <h2>Runoob Test</h2>    
+    <div id="routertest">
+      <p>
+        <!-- 使用 router-link 组件来导航. -->
+        <!-- 通过传入 `to` 属性指定链接. -->
+        <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+        <router-link to="/foo">Go to Foo</router-link>
+        <router-link to="/bar">Go to Bar</router-link>
+      </p>
+      <!-- 路由出口 -->
+      <!-- 路由匹配到的组件将渲染在这里 -->
+      <router-view></router-view>
+    </div>
+
+    <span>页面载入时，input 元素自动获取焦点：</span>
+    <input v-focus><br>
 
     <!-- 判断class1的值，true则使用class1的样式 -->
     <label for="r1">修改颜色</label>
@@ -10,30 +24,14 @@
       directiva v-bind:class
     </div>
 
-    <!-- v-bind绑定class和样式 -->
-    <div v-bind:class="{ active: isActive }"></div>
-    <!-- 可以在对象中传入更多属性用来动态切换多个 class,
-         text-danger 类背景颜色覆盖了 active 类的背景色： -->
-    <div class="static"
-      v-bind:class="{ active: isActive, 'text-danger': hasError }">
-    </div>    
-    <!-- 可以在这里绑定返回对象的计算属性。这是一个常用且强大的模式 -->
-    <div v-bind:class="classObject"></div>
-    <!-- 可以把一个数组传给 v-bind:class -->
-    <div v-bind:class="[activeClass, errorClass]"></div>
-
-    <!-- 直接的div class定义 -->
-    <div class="active"></div>
-    <div class="static active text-danger"></div>
-    <div class="active text-danger"></div>
-
-    <!-- 可以使用三元表达式来切换列表中的 class  -->
-    <div v-bind:class="[errorClass ,isActive ? activeClass : '']"></div>
+    <!-- 自定义指令runoob -->
+    <div v-runoob:hello.ab.="message"></div>
+    <div v-setstyle="{ color: 'gray', text: 'my 指令' }"></div>
 
     <!-- 可以在 v-bind:style 直接设置样式 -->
     <div v-bind:style="{ color: 'green', fontSize: '20px' }">内嵌style</div>
     <!-- 可以直接绑定到一个样式对象，让模板更清晰。style对象在data块中定义-->
-    <div v-bind:style="styleObject">绑定style</div>
+    <div v-bind:style="styleObjectl">绑定style</div>
     <!-- v-bind:style 可以使用数组将多个样式对象应用到一个元素上 -->
     <div v-bind:style="[styleObject, overrideStyle]">style数组</div>
     
@@ -43,7 +41,7 @@
     <pre><a :href="url">this is a link, too</a></pre>
 
     <!-- Vue.js 提供了完全的 JavaScript 表达式支持 -->
-    {{5+5}}
+    {{ 5 + 5 }}
     {{ ok ? 'YES' : 'NO' }}
     {{ message.split('').reverse().join('') }}
     <a v-bind:id="'list-' + id">WTF</a>
@@ -105,9 +103,10 @@
     例如：监听按钮事件，如下触发reverseMessage()，实现字符串反转-->
     <button v-on:click="reverseMessage">反转字符串</button>
     <!-- v-on提供缩写 -->
-    <button @click="reverseMessage">反转2</button>
-    <button @click="count"></button><br>
-
+    <button @click="reverseMessage">反转2</button><br>
+    <button @click="clicker += 1">+1s</button>
+    <a> 总共加了{{clicker}}s</a><br>
+  
     <!-- 过滤器，过滤器函数接受表达式的值作为第一个参数-->
     <!-- 在两个大括号中的过滤器 -->
     {{ message | capitalize }}<br>
@@ -118,10 +117,96 @@
     <!-- 过滤器是 JavaScript 函数，因此可以接受参数 -->
     <!-- {{ message | filterA('arg1', arg2) }} -->
 
+    <p>{{ total }}</p>
+    <!-- 插入组件button-counter，使用 $on(eventName) 监听事件 -->
+    <button-counter v-on:increment="incrementTotal"></button-counter>
+    <button-counter v-on:increment="incrementTotal"></button-counter>
+
+    <!-- v-bind绑定class和样式 -->
+    <div v-bind:class="{ active: isActive }"></div>
+    <!-- 可以在对象中传入更多属性用来动态切换多个 class,
+         text-danger 类背景颜色覆盖了 active 类的背景色： -->
+    <div class="static"
+      v-bind:class="{ active: isActive, 'text-danger': hasError }">
+    </div>    
+    <!-- 可以在这里绑定返回对象的计算属性。这是一个常用且强大的模式 -->
+    <div v-bind:class="classObject"></div>
+    <!-- 可以把一个数组传给 v-bind:class -->
+    <div v-bind:class="[activeClass, errorClass]"></div>
+
+    <!-- 直接的div class定义 -->
+    <div class="active"></div>
+    <div class="static active text-danger"></div>
+    <div class="active text-danger"></div>
+
+    <!-- 可以使用三元表达式来切换列表中的 class  -->
+    <div v-bind:class="[errorClass ,isActive ? activeClass : '']"></div>
+
   </div>
 </template>
- 
+
 <script>
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// 0. 如果使用模块化机制编程，導入Vue和Router，要调用 Vue.use(Router)
+Vue.use(Router)
+// 1. 定义（路由）组件。
+// 可以从其他文件 import 进来
+const Foo = { template: '<div>foo</div>' }
+const Bar = { template: '<div>bar</div>' }
+ 
+// 2. 定义路由
+// 每个路由应该映射一个组件。 其中"component" 可以是
+// 通过 Vue.extend() 创建的组件构造器，
+// 或者，只是一个组件配置对象。
+// 我们晚点再讨论嵌套路由。
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar }
+]
+ 
+// 3. 创建 router 实例，然后传 `routes` 配置
+const router = new Router({
+  routes // （缩写）相当于 routes: routes
+})
+ 
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，从而让整个应用都有路由功能
+const routertest = new Vue({
+  router
+})
+
+Vue.component('button-counter',{
+  template:'<button v-on:click="incrementHandler">{{counter}}</button>',
+  data:function(){
+    return{
+      counter:0
+    } 
+  },
+  methods: {
+    incrementHandler: function () {
+      this.counter += 1
+      // 使用 $emit(eventName) 触发事件
+      this.$emit('increment')
+    }
+  },
+})
+
+// directive注册一个全局自定义指令 v-focus
+// Vue.directive('focus', {
+//   // 当绑定元素插入到 DOM 中。
+//   inserted: function (el) {
+//     // 聚焦元素
+//     el.focus()
+//   }
+// })
+//自定义v-setstyle指令
+Vue.directive('setstyle', function (el, binding) {
+	// 简写方式设置文本及背景颜色
+	el.innerHTML = binding.value.text
+	el.style.backgroundColor = binding.value.color
+})
 
 export default {
   //参数
@@ -130,7 +215,7 @@ export default {
       class1: false,
       vhtml:'v-html test',
       ok: true,
-      message: 'dunoob',
+      message: 'runoob',
       id : 1,
       url:'www.baidu.com',
       type:'C',
@@ -139,6 +224,8 @@ export default {
       error: null,
       activeClass: 'active',
       errorClass: 'text-danger',
+      clicker: 0,
+      total: 0,
       //数组定义格式
       sites: [
       { name: 'Runoob' },
@@ -175,6 +262,9 @@ export default {
       alert('Hello' + this.name + '!')
       if(event)
         alert(greet.target.tagName)
+    },
+    incrementTotal: function () {
+      this.total += 1
     }
   },
   //过滤器
@@ -202,7 +292,7 @@ export default {
       get:function(){
         return this.name + ' ' + this.url
       },
-      //setter
+        //setter
       set:function(newValue){
         var names = newValue.split(' ')
         this.name = newValue[0]
@@ -215,9 +305,40 @@ export default {
         }
       }
     }
+  },
+  //自定义局部指令
+  directives: {
+    // 注册一个局部的自定义指令 v-focus
+    focus: {
+      // 指令的定义
+      inserted: function (el) {
+        // 聚焦元素
+        el.focus()
+      }
+    },
+    runoob: {
+      //钩子函数bind，指令第一次绑定到元素时调用，定义在绑定时执行一次的初始化动作。
+      bind: function (el, binding, vnode) {
+        var s = JSON.stringify
+        el.innerHTML =
+        'name: '       + s(binding.name) + '<br>' +
+        'value: '      + s(binding.value) + '<br>' +
+        'expression: ' + s(binding.expression) + '<br>' +
+        'argument: '   + s(binding.arg) + '<br>' +
+        'modifiers: '  + s(binding.modifiers) + '<br>' +
+        'vnode keys: ' + Object.keys(vnode).join(', ')
+      }
+    },
+    // setstyle: {
+    //   //指令函数可以引入一切JavaScript对象
+    //   function (el, binding) {
+    //   // 简写方式设置文本及背景颜色
+    //   el.innerHTML = binding.value.text
+    //   el.style.backgroundColor = binding.value.color
+    //   }
+    // }
   }
 }
-
 
 </script>
 
